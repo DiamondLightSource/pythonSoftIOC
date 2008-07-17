d3fa5ec55b9d0df07b2aeec8bc24050e4fc05879
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2008-07-17 07:11:19 +0000

    Python soft IOC: Update version definitions
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/pythonSoftIoc@21625 e099a375-04f9-0310-9d5f-a741eaff62e1

--- python/versions.py
+++ python/versions.py
@@ -4,5 +4,6 @@ imported, and should only be used to establish module versions.'''
 import sys
 from pkg_resources import require
 
-require('cothread==1.7')
+require('numpy==1.1.0')
+require('cothread==1.8')
 require('dls.builder==1.3')

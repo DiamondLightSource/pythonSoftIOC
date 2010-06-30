8b84ea2527a314213994887aeb49e096e445752a
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2010-06-30 08:10:49 +0000

    Python soft IOC: Remove numpy version require call
    The numpy library is no longer versioned in our 2.6 installation.
    
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/support/pythonSoftIoc@47383 e099a375-04f9-0310-9d5f-a741eaff62e1

--- python/versions.py
+++ python/versions.py
@@ -4,6 +4,5 @@ imported, and should only be used to establish module versions.'''
 import sys
 from pkg_resources import require
 
-require('numpy==1.3.0')
 require('cothread==1.16')
 require('iocbuilder==3.0')

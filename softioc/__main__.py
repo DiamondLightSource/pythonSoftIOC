f02e08e76a49f12844839fdf7552bbd76caf27c9
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-04-30 14:10:51 +0100

    Change cli to do a subprocess call

--- softioc/__main__.py
+++ softioc/__main__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from argparse import ArgumentParser
+import subprocess
 
 from softioc import __version__
 
@@ -9,16 +10,10 @@ def main(args=None):
     parser = ArgumentParser()
     parser.add_argument("--version", action="version", version=__version__)
     parser.add_argument("script", help="The python script to run")
+    parser.add_argument("arg", help="Any arguments to pass to the script", nargs="*")
     parsed_args = parser.parse_args(args)
-    # Insert the directory containing script onto the path in case we do
-    # any imports
-    sys.path.insert(0, os.path.dirname(os.path.abspath(parsed_args.script)))
-    if sys.version_info < (3, 0):
-        # Python 2
-        execfile(parsed_args.script)
-    else:
-        # Python 3
-        exec(open(parsed_args.script).read())
+    # Execute as subprocess
+    subprocess.Popen([sys.executable, parsed_args.script, *parsed_args.arg]).communicate()
 
 
 if __name__ == "__main__":

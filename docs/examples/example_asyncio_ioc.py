fcbcfcf9e3d2d72127bb4a25896a33a14f983633
AlexWells <alex.wells@diamond.ac.uk>
2021-06-29 11:45:12 +0100

    Update the asyncio example file
    It still doesn't work as I expect - the second caget on AI always returns 5.0, not 999...

--- docs/examples/example_asyncio_ioc.py
+++ docs/examples/example_asyncio_ioc.py
@@ -1,7 +1,6 @@
 # Import the basic framework components.
-import atexit
 from softioc import softioc, builder
-from aioca import caget, caput, _catools
+from aioca import caget, caput
 import asyncio
 
 # Set the record prefix
@@ -10,7 +9,9 @@ builder.SetDeviceName("MY-DEVICE-PREFIX")
 # Create some records
 ai = builder.aIn('AI', initial_value=5)
 
-async def update(val):
+def update(val):
+    print("got here")
+    print(val)
     ai.set(val)
 
 ao = builder.aOut('AO', initial_value=12.45, on_update=update)
@@ -21,10 +22,21 @@ softioc.iocInit()
 
 # Perform some reading/writing to the PVs
 async def do_read_write():
-    ai_val = await caget("MY-DEVICE-PREFIX:AI")
+    print(await caget("MY-DEVICE-PREFIX:AO"))
+    print(await caget("MY-DEVICE-PREFIX:AI"))
     await caput("MY-DEVICE-PREFIX:AO", "999")
+    await asyncio.sleep(10)
+    print(await caget("MY-DEVICE-PREFIX:AI"))
+    print(await caget("MY-DEVICE-PREFIX:AO"))
+
+#asyncio.run(do_read_write())
+
+print(asyncio.run(caget("MY-DEVICE-PREFIX:AI")))
+print(asyncio.run(caget("MY-DEVICE-PREFIX:AO")))
+print(asyncio.run(caput("MY-DEVICE-PREFIX:AO","999")))
+
+print(asyncio.run(caget("MY-DEVICE-PREFIX:AI")))
+print(asyncio.run(caget("MY-DEVICE-PREFIX:AO")))
 
 
-asyncio.run(do_read_write())
-
-
+softioc.interactive_ioc(globals())

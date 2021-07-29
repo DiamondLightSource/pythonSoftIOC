d62a6a0edeadbdfa4feb9e18e5cc22e7b63d2c28
AlexWells <alex.wells@diamond.ac.uk>
2021-07-29 14:14:15 +0100

    Remove trailing whitespace.

--- docs/examples/example_records_calculate.py
+++ docs/examples/example_records_calculate.py
@@ -6,7 +6,7 @@ builder.SetDeviceName("XX-XX-XX-01")
 softioc_record = builder.aOut("VAL1", initial_value=5)
 iocbuilder_record = records.ao("VAL2", VAL=10)
 
-calc = records.calc("CALC", CALC="A*B", SCAN="1 second", 
+calc = records.calc("CALC", CALC="A*B", SCAN="1 second",
                     INPA=softioc_record, INPB=iocbuilder_record)
 
 builder.LoadDatabase()

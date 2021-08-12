6f0fe54a3df33faae37a5b9fdf216c89f398d9ba
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-08-12 11:48:56 +0100

    Allow AsyncioDispatcher to take existing event loop

--- softioc/asyncio_dispatcher.py
+++ softioc/asyncio_dispatcher.py
@@ -3,23 +3,18 @@ import inspect
 import threading
 
 
-class AsyncioDispatcher(threading.Thread):
-    """A dispatcher for `asyncio` based IOCs. Means that `on_update` callback
-    functions can be async. Will run an Event Loop in a thread when
-    created.
-    """
-    def __init__(self):
-        """Create an AsyncioDispatcher suitable to be used by
-        `softioc.iocInit`."""
-        # Docstring specified to suppress threading.Thread's docstring, which
-        # would otherwise be inherited by this method and be misleading.
-        super().__init__()
+class AsyncioDispatcher:
+    def __init__(self, loop=None):
+        """A dispatcher for `asyncio` based IOCs, suitable to be passed to
+        `softioc.iocInit`. Means that `on_update` callback functions can be
+        async. If loop is None, will run an Event Loop in a thread when created.
+        """
         #: `asyncio` event loop that the callbacks will run under.
-        self.loop = asyncio.new_event_loop()
-        self.start()
-
-    def run(self):
-        self.loop.run_forever()
+        self.loop = loop
+        if loop is None:
+            # Make one and run it in a background thread
+            self.loop = asyncio.new_event_loop()
+            threading.Thread(target=self.loop.run_forever).start()
 
     def __call__(self, func, *args):
         async def async_wrapper():

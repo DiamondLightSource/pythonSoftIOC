5c39790e5137f2ce4254dcf3d3dd075a50ad19ee
AlexWells <alex.wells@diamond.ac.uk>
2021-11-25 10:33:07 +0000

    Make script optional+pass everything to subprocess
    This restores previous behaviour of the pythonSoftIOC executable, which
    would give you a Python command line interpreter.

--- softioc/__main__.py
+++ softioc/__main__.py
@@ -9,12 +9,17 @@ from . import __version__
 def main(args=None):
     parser = ArgumentParser()
     parser.add_argument("--version", action="version", version=__version__)
-    parser.add_argument("script", help="The python script to run")
+    parser.add_argument(
+        "script", help="The python script to run", nargs="?", default=None)
     parser.add_argument(
         "arg", help="Any arguments to pass to the script", nargs="*")
-    parsed_args = parser.parse_args(args)
-    # Execute as subprocess
-    cmd = [sys.executable, parsed_args.script] + parsed_args.arg
+    parsed_args, unknown = parser.parse_known_args(args)
+
+    # Execute as subprocess.
+    cmd = [sys.executable] + parsed_args.arg + unknown
+    if parsed_args.script:
+        cmd.insert(1, parsed_args.script)
+
     subprocess.Popen(cmd).communicate()
 
 

cfc13704df3b2a6398767651bbf0023c065c6d1d
AlexWells <alex.wells@diamond.ac.uk>
2021-12-01 08:58:51 +0000

    Remove unnecessary unicode specifiers
    Now that Python2 support is dropped, Python3 strings are unicode anyway

--- tests/test_records.py
+++ tests/test_records.py
@@ -62,12 +62,12 @@ def idfn(fixture_value):
         (builder.stringIn, "", "", str),
         (builder.stringOut, b"abc", "abc", str),
         (builder.stringIn, b"abc", "abc", str),
-        (builder.stringOut, b"a\xcfb", u"a�b", str),  # Invalid UTF-8
-        (builder.stringIn, b"a\xcfb", u"a�b", str),  # Invalid UTF-8
-        (builder.stringOut, b"a\xe2\x82\xacb", u"a€b", str),  # Valid UTF-8
-        (builder.stringIn, b"a\xe2\x82\xacb", u"a€b", str),  # Valid UTF-8
-        (builder.stringOut, u"a€b", u"a€b", str),  # Valid UTF-8
-        (builder.stringIn, u"a€b", u"a€b", str),  # Valid UTF-8
+        (builder.stringOut, b"a\xcfb", "a�b", str),  # Invalid UTF-8
+        (builder.stringIn, b"a\xcfb", "a�b", str),  # Invalid UTF-8
+        (builder.stringOut, b"a\xe2\x82\xacb", "a€b", str),  # Valid UTF-8
+        (builder.stringIn, b"a\xe2\x82\xacb", "a€b", str),  # Valid UTF-8
+        (builder.stringOut, "a€b", "a€b", str),  # Valid UTF-8
+        (builder.stringIn, "a€b", "a€b", str),  # Valid UTF-8
         (
             builder.stringOut,
             "this string is much longer than 40 characters",

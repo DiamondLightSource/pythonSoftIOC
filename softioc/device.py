2c93f3906d669288d62fe1ba248c23d7b57911e5
Michael Abbott <michael.abbott@diamond.ac.uk>
2022-01-12 14:13:07 +0000

    Fix bug in waveform change comparison

--- softioc/device.py
+++ softioc/device.py
@@ -365,7 +365,7 @@ class WaveformBase(ProcessDeviceSupportCore):
         record.NORD = nord
 
     def _compare_values(self, value, other):
-        return numpy.all(value == other)
+        return len(value) == len(other) and numpy.all(value == other)
 
     def _value_to_epics(self, value):
         # Ensure we always convert incoming value into numpy array, regardless

34e5c7d7058d5b96def89afb4b11af67c5c83b63
Araneidae <Araneidae@users.noreply.github.com>
2022-02-03 09:17:28 +0000

    deploy: 53f107d4e375c27af8fdc96017eead873494646d

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.2.1+149.g32929bd',
+    VERSION: '3.2.1+150.g53f107d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

c257a0c19406bc77a24434c064d8f4658d9b2a33
Michael Abbott <michael.abbott@diamond.ac.uk>
2022-08-05 09:09:06 +0100

    Add option for passing an alternative cothread callback dispatcher
    This adds an extra optional argument to CothreadDispatcher.  The only
    realistically sensible alternative argument is probably cothread.Spawn.

--- softioc/cothread_dispatcher.py
+++ softioc/cothread_dispatcher.py
@@ -1,14 +1,23 @@
 
 class CothreadDispatcher:
-    def __init__(self):
+    def __init__(self, dispatcher = None):
         """A dispatcher for `cothread` based IOCs, suitable to be passed to
-        `softioc.iocInit`. """
-        # Import here to ensure we don't instantiate any of cothread's global
-        # state unless we have to
-        import cothread
-        # Create our own cothread callback queue so that our callbacks
-        # processing doesn't interfere with other callback processing.
-        self.__dispatcher = cothread.cothread._Callback()
+        `softioc.iocInit`.  By default scheduled tasks are run on a dedicated
+        cothread callback thread, but an alternative dispatcher can optionally
+        be specified here.  Realistically the only sensible alternative is to
+        pass `cothread.Spawn`, which would create a separate cothread for each
+        dispatched callback.
+        """
+
+        if dispatcher is None:
+            # Import here to ensure we don't instantiate any of cothread's
+            # global state unless we have to
+            import cothread
+            # Create our own cothread callback queue so that our callbacks
+            # processing doesn't interfere with other callback processing.
+            self.__dispatcher = cothread.cothread._Callback()
+        else:
+            self.__dispatcher = dispatcher
 
     def __call__(
             self,

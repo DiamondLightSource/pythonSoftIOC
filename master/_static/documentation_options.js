f61c753cc58053bfb06dda78f4fa22ad64bdc328
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2026-02-04 14:15:01 +0000

    deploy: 3126269c916ca264f0c7c0e72973100b994e7113

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.7.0+3.g09ea964',
+    VERSION: '4.7.0+6.g3126269',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
